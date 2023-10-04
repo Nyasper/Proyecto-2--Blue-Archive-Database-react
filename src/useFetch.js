031d34fda5027eb2b3cd5c0de49aae19b06cc4d8
Nyasper <herreragonzalo39@gmail.com>
2023-10-04 17:50:05 -0300

    useFetch modify

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 
+const local = 'http://localhost:3000'
+const cloud = 'https://proyecto-2-backend.vercel.app'
+
 export function useFetch(url) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     searchData();
   }, []);
-
+  
   async function searchData() {
     try {
-      const response = await fetch(`https://proyecto-2-backend.vercel.app/api/${url}`);
+      const response = await fetch(`${cloud}/api/${url}`);
       const data = await response.json();
       setData(data);
     } catch (error) {
