7c44b79ea48302ba8cb53909c3bce5f6f4c79a2a
Nyasper <herreragonzalo39@gmail.com>
2024-01-16 17:31:36 -0300

    cors fixes

--- server/index.js
+++ server/index.js
@@ -7,15 +7,20 @@ import router from "./routes.js";
 
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://blue-archive-database.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('Proyecto 2 Backend.')
 })
 
-app.use('/api/',router)
+app.use('/api/', router)
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Servidor Abierto http://localhost:${port}`)
 })
 
